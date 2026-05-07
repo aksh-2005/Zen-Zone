8cbee6bd418ebd43f23c1069dedc71f36e42521a
Akash K <akcreation287@gmail.com>
2026-05-08 01:47:20 +0530

    Fixed deployment and database connection

--- zz/zenzone/settings.py
+++ zz/zenzone/settings.py
@@ -5,6 +5,10 @@ Django settings for zenzone project.
 from pathlib import Path
 import os
 import dj_database_url
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
