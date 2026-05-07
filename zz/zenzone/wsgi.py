d188fe84cd103b4a92e0941e9fd902cf8adc3099
Akash K <akcreation287@gmail.com>
2026-05-08 02:33:18 +0530

    Fix Django module path

--- zz/zenzone/wsgi.py
+++ zz/zenzone/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zz.zenzone.settings')
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "zz.zenzone.settings"
+)
 
 application = get_wsgi_application()
