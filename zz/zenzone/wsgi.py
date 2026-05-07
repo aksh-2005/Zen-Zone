8bb1384ca6ebb9fbc5f9f2788aacace96a1d3ef5
Akash K <akcreation287@gmail.com>
2026-05-08 02:29:39 +0530

    test

--- zz/zenzone/wsgi.py
+++ zz/zenzone/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zenzone.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zz.zenzone.settings')
 
 application = get_wsgi_application()
