3de954877515153465b695c63489bd6631eefe3d
Akash K <akcreation287@gmail.com>
2026-05-22 15:58:51 +0530

    chore: enable serving media files via Django views and add static media directory

--- zz/zenzone/urls.py
+++ zz/zenzone/urls.py
@@ -15,13 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include, re_path
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('main.urls')),
-    path('account/',include('django.contrib.auth.urls')),
-  
+    path('', include('main.urls')),
+    path('account/', include('django.contrib.auth.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 handler404 = 'main.views.custom_404'
